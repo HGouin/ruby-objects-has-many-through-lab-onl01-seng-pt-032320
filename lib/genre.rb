8e390390a4d6d21e54c0d6ccb830c82d9cdc24cd
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-23 19:43:29 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -3,7 +3,13 @@ class Genre
 
   attr_reader :name, :song, :artist
 
-  def initialize(name)
+  def initialize(name, artist, song)
     @name = name
+    @artist = artist
+    @song = song
+  end
+
+  def self.all
+    @@all
   end
 end
