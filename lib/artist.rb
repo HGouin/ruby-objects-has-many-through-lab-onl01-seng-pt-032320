b0ba9ed6ccf0fddc6bc3618ff542d072e000487f
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-23 17:38:02 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,4 +1,16 @@
 class Artist
 
   @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(name, genre)
+    new_song = Song.new(name, self, genre)
+  end
 end
