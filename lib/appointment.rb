9324eab0c1315cd7bac9f3734e191d99728abc68
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-24 05:30:34 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,6 +1,6 @@
 class Appointment
   @@all = []
-  attr_reader :date, :patient, :doctor
+  attr_reader :date, :name, :doctor
   def initialize(name, date, doctor)
     @name = name
     @date = date
