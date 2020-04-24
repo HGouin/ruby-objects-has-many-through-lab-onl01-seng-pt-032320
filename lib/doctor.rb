ef174cb14059fd3819af6e5f02c11cfe4c6f7a9d
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-24 05:20:51 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,14 +1,20 @@
 class Doctor
   @@all = []
 
-  attr_reader :name
+  attr_reader :name, :patient
 
-  def initialize(name)
+  def initialize(name, patient)
     @name = name
+    @patient = patient
     @@all << self
   end
 
   def self.all
     @@all
+  end
+
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.doctor == self
   end
 end
